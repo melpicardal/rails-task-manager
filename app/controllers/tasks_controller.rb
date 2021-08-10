b7903becd355b74d968b43200f158781254708e5
meljun picardal <meljun.one@gmail.com>
2021-08-10 15:08:32 +0900

    completed up to #8 in the challenge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,10 @@
 class TasksController < ApplicationController
-
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  def show; end
 
   def new
     @task = Task.new
@@ -18,15 +16,16 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
-  def edit
-    @task = Task.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path
+  end
 
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
@@ -35,4 +34,7 @@ class TasksController < ApplicationController
     params.require(:task).permit(:title, :details, :completed)
   end
 
+  def find_task
+    @task = Task.find(params[:id])
+  end
 end
