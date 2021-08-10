d45779e601ed6f1e3194105bb8bed7834ab3f32b
meljun picardal <meljun.one@gmail.com>
2021-08-10 12:45:43 +0900

    completed up to #6 of challenge (edit/update)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # LIST ALL
+  get 'tasks', to: 'tasks#index'
+  # CREATE
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # LIST ONE
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # UPDATE
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
 end
